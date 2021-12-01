76fd90eb132a9d9588edc5bac49bbcdf78461a40
paul-boegelsack <pboegelsack@googlemail.com>
2021-12-01 12:41:22 +0100

    Updated tests for IotaAddressService

--- src/electron/test/IotaAddressService.test.ts
+++ src/electron/test/IotaAddressService.test.ts
@@ -6,32 +6,53 @@ import {
   Bech32Helper,
 } from '@iota/iota.js';
 import { Bip32Path, Bip39 } from '@iota/crypto.js';
-import { IotaAddressService, Address } from '../lib/IotaAddressService';
+import { SingleNodeClient } from '@iota/iota.js';
+import { MqttClient } from '@iota/mqtt.js';
+import { IotaAddressService } from '../lib/IotaAddressService';
+import { IotaAddress } from '../lib/IotaAddress';
+
+interface BalanceCallback {
+  (topic: string, data: { output: { amount: number } }): void;
+}
+
 describe('IotaNodeService Integration', () => {
   let service;
-  const API = 'api.lb-0.h.chrysalis-devnet.iota.cafe';
+  let nodeClient;
+  let mqttClient;
+  const NODE = 'api.lb-0.h.chrysalis-devnet.iota.cafe';
   const newBechAddress = generateBechAddress();
 
-  beforeEach(async () => {
-    service = new IotaAddressService(API);
+  beforeEach(() => {
+    nodeClient = new SingleNodeClient(`http://${NODE}`);
+    mqttClient = new MqttClient(`mqtt://${NODE}`);
+    service = new IotaAddressService(nodeClient, mqttClient);
   });
 
-  test.only('should receive an address object', async () => {
-    const expectedAddress: Address = {
-      bechAddress: newBechAddress,
-      balance: 0,
-    };
-    const address = await service.GetAddress(expectedAddress.bechAddress);
-    expect(address).toEqual(expectedAddress);
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
-  test('should receive a bech32 address if its balance changed', (done) => {
-    const expectedBechAddress = newBechAddress;
-    service.ListenToAddress(expectedBechAddress, (becAddress: string) => {
-      expect(becAddress).toBe(expectedBechAddress);
-      done();
-    });
-  }, 60000);
+  test('should return an iota address object', async () => {
+    jest
+      .spyOn(mqttClient, 'addressOutputs')
+      .mockImplementation((topic: string, callback: BalanceCallback) => {
+        callback('', { output: { amount: 0 } });
+      });
+    const address = await service.GetAddress(newBechAddress);
+    expect(address).toBeInstanceOf(IotaAddress);
+    expect(address.GetBechAddress()).toBe(newBechAddress);
+    expect(address.GetBalance()).toBe(0);
+  });
+
+  test('should call the balance listener of the returning object if balance of the corresponding address changed', async () => {
+    jest
+      .spyOn(mqttClient, 'addressOutputs')
+      .mockImplementation((topic: string, callback: BalanceCallback) => {
+        callback('', { output: { amount: 200000 } });
+      });
+    const address = await service.GetAddress(newBechAddress);
+    expect(address.GetBalance()).toBe(200000);
+  });
 });
 
 function generateBechAddress() {
