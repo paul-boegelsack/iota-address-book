e33bcbcc18b52b099b4fb7540381483fa9d8e984
paul-boegelsack <pboegelsack@googlemail.com>
2021-12-04 23:35:31 +0100

    Added logo as base64

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,10 +1,3 @@
-<style>	.logo {
-    background-color: white;
-    width: 5vw;
-    -webkit-mask: url('./assets/iota-miota-logo.svg') no-repeat center;
-    mask: url('./assets/iota-miota-logo.svg') no-repeat center;
-}</style>
-
 <div class="logo mr-2" alt="iota logo"></div> 
 <span class="mr-3">|</span>
 <span>ADDRESS BOOK</span>
